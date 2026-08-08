f42e6b40d67ec707e3d34cb1cb59d8fb98f62c7c
sefekcoeg4-debug <sefekcoeg4@gmail.com>
2026-08-08 17:12:29 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+nama = "Faldy Hadyansyah"
+hobi = "Memasak"
+umur = 16 
+laki_laki = True 
+berat_badan = 46.6
+
+print("=== Biodata ===")
+print("Nama:",nama)
+print("Hobi:",hobi)
+print("Umur:",umur,"tahun") 
+print("laki laki:",laki_laki)
+print("Berat Badan:",berat_badan)
