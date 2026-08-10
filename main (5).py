4422899e0488fcb90b87db5725b24612c9710a3d
sefekcoeg4-debug <sefekcoeg4@gmail.com>
2026-08-10 22:16:41 +0700

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,12 @@
+roti = 100
+
+roti += 50
+print("Total jumlah roti pada pagi hari:", roti) 
+roti *= 3
+print("Total jumlah roti setelah dikalikan:", roti) 
+roti -= 120
+print("Total jumlah roti setelah dijual:", roti)
+roti /= 4
+print("Total jumlah roti di setiap etalase:", roti)
+roti += 10
+print("Total jumlah roti per etalase setelah ditambah:", roti)  
